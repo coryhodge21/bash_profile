93fae0edf1994edc73dfd7f9643b423d90da761f
root <root@DESKTOP-IGL83NG>
2023-12-02 11:51:39 -0800

    finally adding to this

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,31 +1,55 @@
 #!/bin/bash
 
-#   File:   ./.bash_profile
+#   File:   .bash_profile.sh
 #   Author: Cory W. Hodge
 #   Date:   12/5/2020   
-
-# PATH EXPORTS
-export PATH="$PATH:"/Applications/microchip/xc8/v2.30/bin""
-
-export PATH="$PATH:"/Applications/microchip/xc32/v2.50/bin""
+#   Description : Handy cmd line functions
 
 ### THIS MACHINE ###
 
 # Navigate to the 
 
 ### NAVIGATION ###
+
+# Enter Directory and Show Contents
 function go {
     cd "$1"     # Open Directory
     clear       # Clear Screen
     ls -la      # List All Items in Long Format
 }
 
+# Go up one directory
 function up {
     cd ..
     clear
 }
 
-function cl {
+# Go up two directories
+function upp {
+    cd ../..
+    clear
+}
+
+# Go up three directories
+function uppp {
+    cd ../../..
+    clear
+}
+
+# Go up four directories
+function upppp {
+    cd ../../../..
+    clear
+}
+
+# Go up five directories
+function uppppp {
+    cd ../../../../..
+    clear
+}
+
+# Clear Screen and List Directory Contents
+function csl {
     clear
     ls -la
 }
